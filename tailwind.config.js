842d2e40d42e81d82bbd2307f635716d5c579f84
Douglas Moura <douglas12345.dsm@gmail.com>
2024-11-21 16:57:08 -0300

    feat: Construção do layout do site em 21-11-2024
    - Menu de navegação
    - Banner
    - Sessão Sobre o Coworking e Lista de "vantagens"
    - Sessão Tipos de serviços
    - Imagens renomeadas e filtradas
    - Cores padrões definidas (verde e roxo)

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ module.exports = {
         extend: {
             fontFamily: {
                 'nunito': ['Nunito', 'Helvetica', 'sans-serif']
+            },
+            colors: {
+                'verde-300': '#61F2C4',
+                'verde-500': '#00D6BA',
+                'verde-700': '#00919A',
+                'roxo-100': '#EBCAFC',
+                'roxo-500': '#6500D6',
+                'roxo-800': '#28007C',
+            },
+            backgroundImage: {
+                'escritorio-banner': "url('../img/escritorio-02.jpg')",
             }
         },
     },
